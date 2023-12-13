e6b7c54208a8aab88bad39c681c5d7712f5784e4
sid-dev-hub <sidsalakki14@gmail.com>
2023-12-13 16:42:15 +0530

    fixed errors

--- src/addition.py
+++ src/addition.py
@@ -1,10 +1,10 @@
 # Simple program for addition, demo of Github actions 
 # Trigger comment 
 
-def add(a,b)
+def add(a,b):
     return a+b
 
-def test()
+def test():
     assert add(1,2) ==3
     assert add(1,-1) == 0
 
